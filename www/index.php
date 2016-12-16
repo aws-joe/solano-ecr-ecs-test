63fbdd63954ad6208d1514b644ee59afe87c5736
aws-joe <fontesj@amazon.com>
2016-12-16 21:09:53 +0000

    new index file title

--- www/index.php
+++ www/index.php
@@ -12,7 +12,7 @@
 # Please see LICENSE.txt for applicable license terms and NOTICE.txt for applicable notices. 
 */
 
-$AppName = "Solano ECR,ECS Demo";
+$AppName = "Solano CI Demo";
 
 ob_start();
 

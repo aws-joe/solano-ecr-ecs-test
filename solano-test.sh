643b412243315a161606b0fa3b90cb24a6036c43
aws-joe <fontesj@amazon.com>
2016-12-24 00:53:26 +0000

    updating test output to print ELB URL

--- solano-test.sh
+++ solano-test.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+export AWS_ACCESS_KEY_ID=$AWS_ASSUME_ROLE_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_ASSUME_ROLE_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN=$AWS_ASSUME_ROLE_SESSION_TOKEN
+
 sudo docker run ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_APP}:${TDDIUM_SESSION_ID} /usr/bin/php /var/www/html/www/index.php
+AWSELBDNS=`aws elb describe-load-balancers --load-balancer-names ${AWS_ELB_NAME} --region ${AWS_DEFAULT_REGION} | jq ".LoadBalancerDescriptions[] | .CanonicalHostedZoneName" | xargs`
+FULL_ELB_DNS="http://${AWSELBDNS}/"
+echo "ELB URL: $FULL_ELB_URL"
